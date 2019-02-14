ce6ff17e7a576f7796d1c3bc3345387c7fa9a2bb
Miller, Payne <pmiller@cardinalsolutions.com>
2019-02-14 09:16:00 -0500

    implement binomial regression

--- src/main/java/stats/Statistician.java
+++ src/main/java/stats/Statistician.java
@@ -4,13 +4,19 @@ import model.Rating;
 import org.apache.spark.api.java.JavaDoubleRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.ml.classification.LogisticRegression;
+import org.apache.spark.ml.classification.LogisticRegressionModel;
+import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.stat.MultivariateStatisticalSummary;
 import org.apache.spark.mllib.stat.Statistics;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import util.RatingsUtil;
 import util.VectorUtil;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Statistician {
@@ -35,5 +41,22 @@ public class Statistician {
         JavaSparkContext sparkContext = JavaSparkContext.fromSparkContext(sparkClient.sparkContext());
         JavaRDD<Vector> distributedDataset = sparkContext.parallelize(VectorUtil.createVectorsFromRatings(ratings));
         return Statistics.colStats(distributedDataset.rdd());
+    }
+
+    public LogisticRegressionModel calculateBinomialRegression(Dataset<Row> dataset) {
+        VectorAssembler assembler = new VectorAssembler()
+                .setInputCols((new String[]{"timestamp"}))
+                .setOutputCol("features");
+
+        Dataset<Row> assembledDataset = assembler.transform(dataset);
+
+        LogisticRegression regression = new LogisticRegression()
+                .setMaxIter(10)
+                .setRegParam(0.3)
+                .setElasticNetParam(0.8)
+                .setLabelCol("timestamp")
+                .setFeaturesCol("features");
+
+        return regression.fit(assembledDataset);
     }
 }

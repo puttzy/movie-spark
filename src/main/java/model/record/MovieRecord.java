c7ef70a3d4752251fb17c86547a1bda7629d192c
Putt, Dan <dputt@cardinalsolutions.com>
2019-02-19 10:06:44 -0500

    removed replaced boilerplate gett sett with @Data annotation from Lombok

--- src/main/java/model/record/MovieRecord.java
+++ src/main/java/model/record/MovieRecord.java
@@ -1,33 +1,13 @@
 package model.record;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class MovieRecord implements Serializable {
     private int movieId;
     private String title;
     private String genres;
 
-    public void setMovieId(int movieId) {
-        this.movieId = movieId;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setGenres(String genres) {
-        this.genres = genres;
-    }
-
-    public int getMovieId() {
-        return movieId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getGenres() {
-        return genres;
-    }
 }
